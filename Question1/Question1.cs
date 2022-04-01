8c95c17225d41ee69868c3c94529087ad433d8f9
michaelwi <michaelwi@DILMICWILT490>
2022-04-01 12:31:54 +0100

    Add project files.

---
+++ Question1/Question1.cs
@@ -0,0 +1,154 @@
+﻿/*
+ * This program has been designed to take a string value (temp_string), before encoding (Cipher.Encode) and decoding (Cipher.Decode) the string value. The encoding and decoding functions are then tested by
+ * comparing the initial string value with itself once it has been both encoded and decoded.
+*/
+
+// If you would like to include the debugging comments that print the encoding and decoding results to the console, un-comment the #define debug variable below.
+//#define debug
+
+using System;
+
+namespace Question1
+{
+    class Question1
+    {
+        private static readonly string temp_string = "Hi, my name is Michael.";
+
+        static void Main(string[] args)
+        {
+            Cipher.Prep();
+            Test();
+        }
+
+        private static void Test()
+        {
+            if (String.Equals(temp_string, Cipher.Decode(Cipher.Encode(temp_string)), StringComparison.InvariantCulture))
+            {
+                Console.WriteLine("Test succeeded");
+            }
+            else
+            {
+                Console.WriteLine("Test failed");
+            }
+        }
+    }
+
+    class Cipher
+    {
+        private static readonly char[] transcode = new char[64];
+
+        public static char[] Prep()
+        {
+            for (int i = 0; i < (transcode.Length - 3); i++)
+            {
+                transcode[i] = (char)((int)'A' + i);
+                if (i > 25) transcode[i] = (char)((int)transcode[i] + 6);
+                if (i > 51) transcode[i] = (char)((int)transcode[i] - 0x4b);
+            }
+            transcode[(transcode.Length - 3)] = '+';
+            transcode[(transcode.Length - 2)] = '/';
+            transcode[(transcode.Length - 1)] = '=';
+
+            return transcode;
+        }
+
+        public static string Encode(string input)
+        {
+            int l = input.Length;
+            int cb = (l / 3 + (Convert.ToBoolean(l % 3) ? 1 : 0)) * 4;
+
+            char[] output = new char[cb];
+            for (int i = 0; i < cb; i++)
+            {
+                output[i] = '=';
+            }
+
+            int c = 0;
+            int reflex = 0;
+            const int s = 0x3f;
+
+            for (int j = 0; j < l; j++)
+            {
+                reflex <<= 8;
+                reflex &= 0x00ffff00;
+                reflex += input[j];
+
+                int x = ((j % 3) + 1) * 2;
+                int mask = s << x;
+                while (mask >= s)
+                {
+                    int pivot = (reflex & mask) >> x;
+                    output[c++] = transcode[pivot];
+                    int invert = ~mask;
+                    reflex &= invert;
+                    mask >>= 6;
+                    x -= 6;
+                }
+            }
+
+            switch (l % 3)
+            {
+                case 1:
+                    reflex <<= 4;
+                    output[c++] = transcode[reflex];
+                    break;
+
+                case 2:
+                    reflex <<= 2;
+                    output[c++] = transcode[reflex];
+                    break;
+
+            }
+#if debug
+            Console.WriteLine("[DEBUG] The test string (" + input + ") has been encoded: " + new string(output) + "\n");
+#endif
+            return new string(output);
+        }
+
+        public static string Decode(string input)
+        {
+            int l = input.Length;
+            int cb = (l / 4 + ((Convert.ToBoolean(l % 4)) ? 1 : 0)) * 3 + 1;
+            char[] output = new char[cb];
+            int c = 0;
+            int bits = 0;
+            int reflex = 0;
+            for (int j = 0; j < l; j++)
+            {
+                reflex <<= 6;
+                bits += 6;
+                bool fTerminate = ('=' == input[j]);
+                if (!fTerminate)
+                    reflex += IndexOf(input[j]);
+
+                while (bits >= 8)
+                {
+                    int mask = 0x000000ff << (bits % 8);
+                    output[c++] = (char)((reflex & mask) >> (bits % 8));
+                    int invert = ~mask;
+                    reflex &= invert;
+                    bits -= 8;
+                }
+
+                if (fTerminate)
+                    break;
+            }
+#if debug
+            Console.WriteLine("[DEBUG] The dencoded test string (" + input + ") has been decoded: " + new string(output) + "\n");
+#endif
+            return new string(output);
+        }
+
+        private static int IndexOf(char ch)
+        {
+            int index;
+            for (index = 0; index < transcode.Length; index++)
+                if (ch == transcode[index])
+                    break;
+            return index;
+        }
+    }
+
+}
+
+
---
+++ Question2/Question2.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * This Higher or Lower game has been designed to take an integer value for the number of decks used in the game (numberOfDeck), and an integer value for the number of cards per suit (newSuitSize), 
+ * before selecting a random card for the player and opponent, and declaring a winner.
+*/
+
+// If you would like to include the debugging comments that print the encoding and decoding results to the console, un-comment the #define debug variable below.
+//#define debug
+
+using System;
+
+namespace Question2
+{
+    class Question2
+    {
+        private static readonly int numberOfDecks = 1;
+        private static readonly int newSuitSize = 13;
+
+        static void Main(string[] args)
+        {
+            Deck newDeck = new(numberOfDecks, newSuitSize);
+            HighCard.Play(newDeck);
+        }
+    }
+
+    class Deck
+    {
+        private  readonly List<KeyValuePair<string, int>> tempDeck;
+        private readonly string[] suits = { "Spades", "Clubs", "Hearts", "Diamonds" };
+        private readonly int suitSize;
+        private readonly int deckSize;
+        private readonly int deckNum;
+
+        public Deck(int numberOfDecks, int newSuitSize)
+        {
+            suitSize = newSuitSize;
+            deckSize = newSuitSize * suits.Length + 1;
+            deckNum = numberOfDecks;
+#if debug
+            Console.WriteLine("[DEBUG] The size of the game's deck is: " + (deckNum * deckSize) + "\n");
+#endif
+            tempDeck = new List<KeyValuePair<string, int>>(deckNum * deckSize);
+
+
+            for (int i = 0; i < deckNum; i++) { BuildDeck(); }
+
+            tempDeck.Add(new KeyValuePair<string, int>("Joker", tempDeck.Count + 1));
+#if debug
+            Console.WriteLine("[DEBUG] The cards in the deck:");
+            foreach (KeyValuePair<string, int> card in tempDeck) { Console.WriteLine(card); }
+            Console.WriteLine("\n");
+#endif
+        }
+
+        private void BuildDeck()
+        {
+            int value = 1;
+
+            for (int j = 0; j < suitSize; j++)
+            {
+                foreach (string suit in suits)
+                {
+                    int cardValue = j % suitSize + 1;
+                    switch (cardValue)
+                    {
+                        case 1:
+                            tempDeck.Add(new KeyValuePair<string, int>("Ace of " + suit, value++));
+                            break;
+                        case 11:
+                            tempDeck.Add(new KeyValuePair<string, int>("Jack of " + suit, value++));
+                            break;
+                        case 12:
+                            tempDeck.Add(new KeyValuePair<string, int>("Queen of " + suit, value++));
+                            break;
+                        case 13:
+                            tempDeck.Add(new KeyValuePair<string, int>("King of " + suit, value++));
+                            break;
+                        default:
+                            tempDeck.Add(new KeyValuePair<string, int>(cardValue + " of " + suit, value++));
+                            break;
+                    }
+                }
+            }
+        }
+
+        public List<KeyValuePair<string, int>> GetDeck()
+        {
+            return tempDeck;
+        }
+    }
+
+    class HighCard
+    {
+        public static void Play(Deck newDeck)
+        {
+            List<KeyValuePair<string, int>> gameDeck = newDeck.GetDeck();
+            Random rnd = new();
+
+            KeyValuePair<string, int> playerCard = gameDeck[rnd.Next(1, gameDeck.Count) ];
+            KeyValuePair<string, int> opponentCard = gameDeck[rnd.Next(1, gameDeck.Count) ];
+
+            Console.WriteLine("Player has " + playerCard.Key + ". \nOpponent has " + opponentCard.Key + ".\n");
+
+            if (playerCard.Value > opponentCard.Value) { Console.WriteLine("Player 1 Wins."); }
+            else if (playerCard.Value < opponentCard.Value) { Console.WriteLine("Opponent Wins."); }
+            else if (playerCard.Value == opponentCard.Value)
+            {
+                Console.WriteLine("It's a draw. \n\nNew cards will be dealt.");
+                Play(newDeck);
+            }
+            else { Console.WriteLine("An error has occured, please close the game."); }
+#if debug
+            Console.WriteLine("\n[DEBUG] Player 1 has " + playerCard.Key + " with a value of " + playerCard.Value + ", and oppenent has " + opponentCard.Key + " with a value of " + opponentCard.Value + ".");
+#endif
+        }
+    }
+}
+
+
